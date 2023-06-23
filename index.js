c6eaea3b61fd2bd241b9e9848a83133404821869
7020530266 <1999shubhamjoshi@gmail.com>
2023-06-23 23:57:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin,
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic
+
+  let input = userInput[0];
+  let string = 0;
+  let areaType = ["Theatre", "Pub", "Commercial Park"];
+  let price = [1500, 1000, 3000];
+  let area = [5, 4, 10];
+  let profitArray = [];
+  let noOfAreaCounts = [];
+
+  for (let j = 0; j < area.length; j++) {
+    for (let i = 1; input >= area[j] * i; i++) {
+      let add = (input - parseInt(area[j]) * i) * price[j];
+      string = string + add;
+
+      if (input < area[j] * (i + 1)) {
+        noOfAreaCounts.push(i);
+        profitArray.push(string);
+      }
+    }
+
+    string = 0;
+  }
+
+  var unique = [...new Set(profitArray)];
+  let maxEle = Math.max(...profitArray);
+
+  let uniIndex = profitArray.indexOf(maxEle);
+  let empty = [0, 0, 0];
+
+  if (unique.length === areaType.length) {
+    empty[uniIndex] = noOfAreaCounts[uniIndex];
+    console.log(
+      "T:" + empty[0] + " " + "P:" + empty[1] + " " + "C:" + empty[2]
+    );
+  }
+
+  if (unique.length === 2) {
+    let firstIndex = profitArray.firstIndexOf(maxEle);
+    let lastIndex = profitArray.lastIndexOf(maxEle);
+
+    empty[firstIndex] = noOfAreaCounts[firstIndex];
+    empty[lastIndex] = noOfAreaCounts[lastIndex];
+
+    if (maxEle === profitArray[0]) {
+      console.log("T:" + empty[0] + " " + "P:" + "0" + " " + "C:" + "0");
+    }
+    if (maxEle === profitArray[1]) {
+      console.log("T:" + "0" + " " + "P:" + empty[1] + " " + "C:" + "0");
+    }
+    if (maxEle === profitArray[3]) {
+      console.log("T:" + "0" + " " + "P:" + "0" + " " + "C:" + empty[2]);
+    }
+  }
+
+  if (unique.length === 1) {
+    if (maxEle === profitArray[0]) {
+      console.log(
+        "T:" + noOfAreaCounts[0] + " " + "P:" + "0" + " " + "C:" + "0"
+      );
+    }
+    if (maxEle === profitArray[1]) {
+      console.log(
+        "T:" + "0" + " " + "P:" + noOfAreaCounts[1] + " " + "C:" + "0"
+      );
+    }
+    if (maxEle === profitArray[3]) {
+      console.log(
+        "T:" + "0" + " " + "P:" + "0" + " " + "C:" + noOfAreaCounts[2]
+      );
+    }
+  }
+});
